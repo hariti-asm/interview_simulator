afb45ff69b41516a0e509c16ccdc8fabd598f34a
Asmaa Hariti <haritiasmaa74@gmail.com>
2025-02-13 13:52:00 +0100

    improve hero section

--- frontend/simulator/src/app/app.component.ts
+++ frontend/simulator/src/app/app.component.ts
@@ -7,14 +7,17 @@ import {StatsComponent} from './components/stats/stats.component';
 import {TestimonialsComponent} from './components/testimonials/testimonials.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {CtaComponent} from './components/cta/cta.component';
+import {UsageComponent} from './components/usage/usage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeaturesComponent, StatsComponent, TestimonialsComponent, FooterComponent, CtaComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeaturesComponent, StatsComponent, TestimonialsComponent, FooterComponent, CtaComponent, UsageComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  seo="seo.png";
+
   title = 'simulator';
 }
